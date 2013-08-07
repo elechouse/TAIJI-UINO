54720e447b68a12e445bc3cc82cf55419d62b34e
JiapengLi <gapleehit@gmail.com>
2013-08-07 16:36:45 +0800

    Add "additional pins" section and example for it.

--- README.md
+++ README.md
@@ -23,7 +23,72 @@ Sample code to demonstrate how to use Arduino DUE
 
 	![Taijidiag](./image/taijidiag1.jpg)
 
+- **additional_pins** shows how to use TaijiUino additional pins.
 
+
+## Additional Pins
+As what you see, there are more pins on Taijiuino DUE board. On the backside some silkscreen is marked aside them.  
+Because Arduino IDE does not support these pins, so to use these pins, you need do some hack things to enable them. 
+
+### Patch Arduino IDE
+- Download the **TAIJI-UINO** repository.
+- In `patch/additional-pins-patch` folder, find `variant.cpp` and `variant.h`
+- Open your Arduino IDE folder, and open `$ARDUINO/hardware/arduino/sam/variants/arduino_due_x` ($ARDUINO stands for the position of your Arduino IDE).
+- In the `arduino_due_x` folder you can see `variant.cpp` and `variant.h`, rename this two files to `variant.cpp.bak` and `variant.h.bak`, in case of you want go back to the origin IDE.
+- Copy the `patch/additional-pins-patch/{variant.cpp, variant.h}` to `arduino_due_x` folder
+- Open `software/additional_pins/additional_pins.ino`, upload it to your Taijiuino DUE, then test it.
+
+### Why need hack?
+We aim at bringing you more pins, but origin Arduino IDE does not support it, so we just hack it.
+
+### What change?
+In `variant.cpp`, add description of the additional pins to the structure ` PinDescription g_APinDescription[]`, number of these pins are from 92 to 109.  
+In `variant.h`, define macros help to remember the index value.
+Access these pins use three ways(name, pin number, or alias):
+
+	Name				Pin Number				 Alias
+	PB24					103					  TP0
+	PB23					104					  TP1
+	PB22					105					  TP2
+	PB27					106					  TP3
+	PC20					107					  TP4
+	PC10					108					  TP5
+	PC11					109					  TP6
+	---------------------------------------------------
+	ERXD1   				98					  TP7
+	ETXD1   				95					  TP8
+	ERXER   				99				      TP9
+	ERXDV  					96					  TP10
+	EMDC    				100					  TP11
+	ERXD0   				97					  TP12
+	EMDIO   				101					  TP13
+	ETXD0   				94					  TP14
+	ETXEN   				93					  TP15
+	EXINT   				102					  TP16
+	ETXCLK  				92					  TP17
+	CLKEN   				109					  TP6
+
+Details in `variant.cpp` and `variant.h`.
+
+### Example
+	void setup(void)
+	{
+		// All this three lines do the same thing
+		pinMode(PB23, OUTPUT);
+		pinMode(103, OUTPUT);
+		pinMode(TP0, OUTPUT);
+
+		// All this three lines do the same thing
+		digitalWrite(PB23, HIGH);
+		digitalWrite(103, HIGH);
+		digitalWrite(TP0, HIGH);
+	}
+
+	void loop(void)
+	{
+		//do nothing
+	}
+	
 ## Get files ##
 
     # get through GIT tools
@@ -32,6 +97,9 @@ or
 
 	# download the zip file
 
-## Buy a board ##
+## Buy
+[![elechouse][EHICON]][EHLINK]
 
-[On Ebay](http://www.ebay.com/itm/TAIJIUINO-Due-Pro-Board-with-Programmer-completely-compatible-with-Arduino-Due-/140903497573?pt=LH_DefaultDomain_0&hash=item20ce80bf65) or [On Elechouse website](http://www.elechouse.com/elechouse/index.php?main_page=product_info&cPath=72_73&products_id=2214)
+[EHLINK]: http://www.elechouse.com
+
+[EHICON]: https://raw.github.com/elechouse/CarDriverShield/master/image/elechouse.png
---
+++ software/additional_pins/additional_pins.ino
@@ -0,0 +1,197 @@
+/**
+ ******************************************************************************
+ * @file    additional_pins.ino
+ * @author  JiapengLi
+ * @brief   This file provides a demostration on 
+ * how to use Taijiuino DUE addtional IO pin.
+ ******************************************************************************
+ * @note:
+ *  Input '0' to flip TaijiPin0, '1' for TaijiPin1, 2 for TaijiPin2 and so on.
+ *  This example is only used for TaijiUino DUE
+ ******************************************************************************
+ * @section  HISTORY
+ * 
+ * 2013/08/6    Initial version.
+ */
+
+/** save pin status */
+uint32_t taiji_pin;
+
+void setup() {
+  // Serial initialize Native USB serial and USART0
+  SerialUSB.begin(115200);
+  Serial.begin(115200);
+
+  pinMode(TP0, OUTPUT);
+  pinMode(TP1, OUTPUT);
+  pinMode(TP2, OUTPUT);
+  pinMode(TP3, OUTPUT);
+  pinMode(TP4, OUTPUT);
+  pinMode(TP5, OUTPUT);
+  pinMode(TP6, OUTPUT);
+
+  digitalWrite(TP0, HIGH);
+  digitalWrite(TP1, HIGH);
+  digitalWrite(TP2, HIGH);
+  digitalWrite(TP3, HIGH);
+  digitalWrite(TP4, HIGH);
+  digitalWrite(TP5, HIGH);
+  digitalWrite(TP6, HIGH);
+
+  taiji_pin |= 0x7F;
+}
+
+void loop() {
+  // Control through USB Serial
+  if(SerialUSB.available()){
+    switch(SerialUSB.read()){
+    case '0':
+      if(taiji_pin & (1<<0)){
+        taiji_pin &= ~(1<<0);
+        digitalWrite(TP0, LOW);
+      }
+      else{
+        taiji_pin |= (1<<0);
+        digitalWrite(TP0, HIGH);
+      }
+      break;
+    case '1':
+      if(taiji_pin & (1<<1)){
+        taiji_pin &= ~(1<<1);
+        digitalWrite(TP1, LOW);
+      }
+      else{
+        taiji_pin |= (1<<1);
+        digitalWrite(TP1, HIGH);
+      }
+      break;
+    case '2':
+      if(taiji_pin & (1<<2)){
+        taiji_pin &= ~(1<<2);
+        digitalWrite(TP2, LOW);
+      }
+      else{
+        taiji_pin |= (1<<2);
+        digitalWrite(TP2, HIGH);
+      }
+      break;
+    case '3':
+      if(taiji_pin & (1<<3)){
+        taiji_pin &= ~(1<<3);
+        digitalWrite(TP3, LOW);
+      }
+      else{
+        taiji_pin |= (1<<3);
+        digitalWrite(TP3, HIGH);
+      }
+      break;
+    case '4':
+      if(taiji_pin & (1<<4)){
+        taiji_pin &= ~(1<<4);
+        digitalWrite(TP4, LOW);
+      }
+      else{
+        taiji_pin |= (1<<4);
+        digitalWrite(TP4, HIGH);
+      }
+      break;
+    case '5':
+      if(taiji_pin & (1<<5)){
+        taiji_pin &= ~(1<<5);
+        digitalWrite(TP5, LOW);
+      }
+      else{
+        taiji_pin |= (1<<5);
+        digitalWrite(TP5, HIGH);
+      }
+      break;
+    case '6':
+      if(taiji_pin & (1<<6)){
+        taiji_pin &= ~(1<<6);
+        digitalWrite(TP6, LOW);
+      }
+      else{
+        taiji_pin |= (1<<6);
+        digitalWrite(TP6, HIGH);
+      }
+      break;
+    }
+  }
+
+  // Control through USART
+  if(Serial.available()){
+    switch(Serial.read()){
+    case '0':
+      if(taiji_pin & (1<<0)){
+        taiji_pin &= ~(1<<0);
+        digitalWrite(TP0, LOW);
+      }
+      else{
+        taiji_pin |= (1<<0);
+        digitalWrite(TP0, HIGH);
+      }
+      break;
+    case '1':
+      if(taiji_pin & (1<<1)){
+        taiji_pin &= ~(1<<1);
+        digitalWrite(TP1, LOW);
+      }
+      else{
+        taiji_pin |= (1<<1);
+        digitalWrite(TP1, HIGH);
+      }
+      break;
+    case '2':
+      if(taiji_pin & (1<<2)){
+        taiji_pin &= ~(1<<2);
+        digitalWrite(TP2, LOW);
+      }
+      else{
+        taiji_pin |= (1<<2);
+        digitalWrite(TP2, HIGH);
+      }
+      break;
+    case '3':
+      if(taiji_pin & (1<<3)){
+        taiji_pin &= ~(1<<3);
+        digitalWrite(TP3, LOW);
+      }
+      else{
+        taiji_pin |= (1<<3);
+        digitalWrite(TP3, HIGH);
+      }
+      break;
+    case '4':
+      if(taiji_pin & (1<<4)){
+        taiji_pin &= ~(1<<4);
+        digitalWrite(TP4, LOW);
+      }
+      else{
+        taiji_pin |= (1<<4);
+        digitalWrite(TP4, HIGH);
+      }
+      break;
+    case '5':
+      if(taiji_pin & (1<<5)){
+        taiji_pin &= ~(1<<5);
+        digitalWrite(TP5, LOW);
+      }
+      else{
+        taiji_pin |= (1<<5);
+        digitalWrite(TP5, HIGH);
+      }
+      break;
+    case '6':
+      if(taiji_pin & (1<<6)){
+        taiji_pin &= ~(1<<6);
+        digitalWrite(TP6, LOW);
+      }
+      else{
+        taiji_pin |= (1<<6);
+        digitalWrite(TP6, HIGH);
+      }
+      break;
+    }
+  }
+}
+
